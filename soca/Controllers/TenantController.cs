a4cbb4a5a1b35314373a4a45b8383f8fdcf64065
fxr1989 <felixramirez19892015@gmail.com>
2022-03-02 22:30:42 -0600

    Servicio Tenant

---
+++ soca/Controllers/TenantController.cs
@@ -0,0 +1,27 @@
+﻿using soca.Controllers.Base;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.Contratos;
+using Servicios;
+
+namespace soca.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TenantController : BaseController
+    {
+        public ITenantServicio Servicio { get; set; }
+
+        public TenantController(SocaContext context) : base(context)
+        {
+            Servicio = new TenantServicio(context);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var resultado = Servicio.ObtenerTodos();
+            return Ok(resultado);
+        }
+    }
+}
